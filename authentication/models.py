9986b043384830b5026a096e2ce1ac4079a849fa
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2025-07-25 10:57:16 +1000

    [FEAT] enabled register and login for admins

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 class Imagerecord(models.Model):
     file_name = models.TextField()
--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Imagerecord
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
+from rest_framework import permissions, status
 from authentication.serializers import UserSerializer
 
 
@@ -10,11 +10,12 @@ def imagerecord_list(request):
     records = Imagerecord.objects.all()
     return render(request, 'imagerecorjd_list.html', {'records': records})
 
+
 class Register(APIView):
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAdminUser]
 
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=201)
+            serializer = UserSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=201)
